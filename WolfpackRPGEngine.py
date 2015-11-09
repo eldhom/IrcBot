c4998992f66f92e760d50d30867e46cec690a3af
eldhom <wass95@hotmail.se>
2015-11-09 18:50:58 +0100

    fixed some things

--- WolfpackRPGEngine.py
+++ WolfpackRPGEngine.py
@@ -11,7 +11,16 @@ class WolfpackRPGEngine(MessageHandler.MessageHandler):
 			'You are already queued in the Group Finder! Type !queuetime for more information.\r\n',
 		]
 		self._removedFromQueueMessage = [
-			'You were removed from the Group Finder.\r\n'
+			'You were removed from the Group Finder.\r\n',
+			'Your party has been disbanded.\r\n'
+		]
+		
+		self._inPartyMessage = [
+			'You\'ve been matched for'
+		]
+		
+		self._disbandPartyMessage = [
+			'Your party has been disbanded.'
 		]
 		
 	def onMessage(self, data):
@@ -21,12 +30,22 @@ class WolfpackRPGEngine(MessageHandler.MessageHandler):
 		message = data[2][1]
 		if data[1] == 'WHISPER':
 			if nick == 'lobotjr':
-				if message in self._inQueueMessage:
-					self._queued 	= True
-				elif message in self._removedFromQueueMessage:
-					self._queued	= False
+				for line in self._inQueueMessage:
+					if message.find(line) != -1:
+						self._queued = True
+				for line in self._removedFromQueueMessage:
+					if message.find(line) != -1:
+						self._queued = False
+
+				for line in self._inPartyMessage:
+					if message.find(line) != -1:
+						self._inParty = True
+				for line in self._disbandPartyMessage:
+					if message.find(line) != -1:
+						self._inParty = False
 			
 				if not self._queued:
 					replyType		= 'PRIVMSG'
 					replyMessage 	= '#jtv :/w lobotjr !queue 1'
+
 		return replyType, replyMessage
